ed3e4b1fa88628514ca0409a1483b7bb4f5e5ef5
Michael Houmann <michael@michael-h.dk>
2017-01-24 19:20:51 +0100

    Added SASS and Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var gulp = require('gulp'),
     gutil= require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass') ;
 
-var coffeeSources = ['components/coffee/*.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSourses = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/templates.js'
 ];
+var sassScourses = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -24,4 +26,15 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassScourses)
+    .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
